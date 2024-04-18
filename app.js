08d145bebc4d76f9e0e44830748199ce17735a89
TedrosTK <tedydada@gmail.com>
2024-04-18 16:19:12 -0400

    Initial Commit

---
+++ app.js
@@ -0,0 +1,34 @@
+//console.log(fetch("https://jsonplaceholder.typicode.com/users/1"))
+const emailRef = document.querySelector(".email");
+// console.log(emailRef)
+// console.log(emailRef)
+// 1. Then
+
+// fetch("https://jsonplaceholder.typicode.com/users/1").then(response => {
+//     response.json().then(data => {
+//         console.log(data)
+//         emailRef.innerHTML = data.email;
+//     })
+// })
+
+
+
+// fetch("https://jsonplaceholder.typicode.com/users/1")
+// .then((response) => {
+//     return response.json();
+// })
+// .then((data) => {
+//     console.log(data);
+//     emailRef.innerHTML = data.email
+// });
+
+// 2. Async / Await
+
+async function main() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users/1")
+    const data = response.json()
+    console.log(data)
+    emailRef.innerHTML = data.name
+}
+
+main();
